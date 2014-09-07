c6a2e247274c7ef13cb1fbd02cfba19e9dce0350
Joel Stanley <joel@jms.id.au>
2014-09-08 00:13:49 +0930

    Skeleton of Flask app
    Performs auth to Strava without any error checking, and displays the
    first shoe and bike that the athlete has.
    
    Requires env variable FIXSTRAVA_CONFIG with SECRET_KEY, CLIENT_SECRET,
    and CLIENT_ID set. The key is a random string to encrypt client cookies,
    the other two are Strava API secrets.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>

---
+++ app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import flask
+import logging
+import stravalib
+
+app = flask.Flask(__name__)
+app.config.from_envvar('FIXSTRAVA_CONFIG')
+
+logging.basicConfig(level=logging.INFO)
+
+app.secret_key = SECRET_KEY
+
+@app.route('/')
+def homepage():
+    if 'access_token' not in flask.session:
+        return flask.redirect(flask.url_for('login'))
+    client = stravalib.client.Client(access_token=flask.session['access_token'])
+    athlete = client.get_athlete()
+
+    return '<h1>Hello {}</h1> <p>Shoe: {}</p><p>Bike: {}</p>'.format(
+            athlete.firstname, athlete.shoes[0].name, athlete.bikes[0].name
+            )
+
+@app.route('/login')
+def login():
+    client = stravalib.client.Client()
+    auth_url = client.authorization_url(client_id=CLIENT_ID,
+            redirect_uri='http://127.0.0.1:7123/auth')
+    return '<a href="{}">Authenticate with Strava</a>'.format(auth_url)
+
+@app.route('/auth')
+def auth_done():
+    # TODO: check for errors 
+    code = flask.request.args.get('code', '')
+    client = stravalib.client.Client()
+    token = client.exchange_code_for_token(client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            code = code)
+    flask.session['access_token'] = token
+    print "Got token {}".format(token)
+    return flask.redirect(flask.url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True, port=7123)
